036f053f87beba8b45a39ea35297ee0792a64295
Samuel Guard <sg2268@bath.ac.uk>
2022-01-08 11:05:09 +0000

    test

--- main.c
+++ main.c
@@ -276,9 +276,11 @@ void manager(const unsigned int width, const unsigned int height,
     g.height = height;
     generate_grid(&g, PATTERN_GRADIENT);
 
+    return;
+
     // Initalise each worker with grid
     send_entire_grid_to_workers(gap, n_workers, &g, g_data_array);
-    return;
+    
     // Main loop
     unsigned int row1, row2;
     int is_finished = FALSE;
@@ -357,6 +359,8 @@ void worker(const unsigned int my_rank) {
 
     const double precision = init_data.precision;
 
+    return;
+
     // Get starting values of the grid
     MPI_Recv(g0.val, g0.width * g0.height, MPI_DOUBLE, 0, TAG_GRID_DATA,
              MPI_COMM_WORLD, &stat);
@@ -371,7 +375,7 @@ void worker(const unsigned int my_rank) {
         g1.val[y * g0.width] = g0.val[y * g0.width];
         g1.val[(y + 1) * g0.width - 1] = g0.val[(y + 1) * g0.width - 1];
     }
-    return;
+    
     int is_finished = FALSE, is_less_than_prec;
     while (TRUE) {
         // See if program is finished
